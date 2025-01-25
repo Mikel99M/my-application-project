b651bbd8cca3a3130723df58effc9377670e5364
Michał Mielcarek <mike249mielcarek@gmail.com>
2025-01-25 12:26:19 +0100

    First push

---
+++ src/main/java/extra/Main.java
@@ -0,0 +1,17 @@
+package extra;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ZadanieDodatkowe17_3 task = new ZadanieDodatkowe17_3();
+        int[] grades = {3,1,1,5,6,4};
+        int[] weights = {4,6,8,4,4,10};
+
+
+        try {
+            System.out.println(task.calculateWeightedAverage(grades, weights));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/extra/ZadanieDodatkowe17_3.java
@@ -0,0 +1,42 @@
+package extra;
+
+import java.util.Arrays;
+
+public class ZadanieDodatkowe17_3 {
+
+    public double calculateWeightedAverage(int[] grades, int[] weights) {
+
+        if (grades.length != weights.length) {
+            throw new IllegalArgumentException("invalid input: grades and weights are not equal");
+        }
+
+        if (grades.length == 0 && weights.length == 0) {
+            throw new IllegalArgumentException("invalid input: grades and weights are empty");
+        }
+
+        for (int grade : grades) {
+            if (grade < 1 || grade > 6) {
+                throw new IllegalArgumentException("invalid input: grade must be between 1 and 6");
+            }
+        }
+
+        for (int weight : weights) {
+            if (weight < 1 || weight > 10) {
+                throw new IllegalArgumentException("invalid input: weight must be between 1 and 10");
+            }
+        }
+
+        return excecuteCalculation(grades, weights);
+    }
+
+    private double excecuteCalculation(int[] grades, int[] weights) {
+
+        double result = 0;
+
+        for (int i = 0; i < grades.length; i++) {
+            result += (grades[i] * weights[i]);
+        }
+
+        return result / Arrays.stream(grades).sum();
+    }
+}
