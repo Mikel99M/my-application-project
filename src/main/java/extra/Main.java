ddb3eec776b0a3502e67f800f9650059d297e9c1
Michał Mielcarek <mike249mielcarek@gmail.com>
2025-01-29 17:26:26 +0100

    Zadanie 17.4

--- src/main/java/extra/Main.java
+++ src/main/java/extra/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
         ZadanieDodatkowe17_3 task = new ZadanieDodatkowe17_3();
         int[] grades = {3,1,1,5,6,4};
-        int[] weights = {4,6,8,4,4,10};
+        int[] weights = {4,6,8,4,4,109};
 
 
         try {
--- src/main/java/extra/ZadanieDodatkowe17_3.java
+++ src/main/java/extra/ZadanieDodatkowe17_3.java
@@ -1,10 +1,11 @@
 package extra;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 public class ZadanieDodatkowe17_3 {
 
-    public double calculateWeightedAverage(int[] grades, int[] weights) {
+    public double calculateWeightedAverage(int[] grades, int[] weights) throws IOException {
 
         if (grades.length != weights.length) {
             throw new IllegalArgumentException("invalid input: grades and weights are not equal");
